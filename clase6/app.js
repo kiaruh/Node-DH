131e9bd94621b8f0dc5f4110b3de50630fb34ff0
Qiyin li <qiyinli@Qiyins-MacBook-Pro.local>
2021-04-18 20:15:48 -0300

    agregue codigo de dh

--- clase6/app.js
+++ clase6/app.js
@@ -1,20 +1,43 @@
-let notas = [10,4,5,6,8,7,9,2];
+// let notas = [10,4,5,6,8,7,9,2];
 
-let notasPor100 = notas.map(function(n){
-    return n*100;
-});
+// let notasPor100 = notas.map(function(n){
+//     return n*100;
+// });
 
-console.log(notasPor100);
+// console.log(notasPor100);
 
-let notasAprobadas = notas.filter(n=>n>=7);
+// let notasAprobadas = notas.filter(n=>n>=7);
 
-console.log(notasAprobadas);
+// console.log(notasAprobadas);
 
-let sumasNotas = notas.reduce((a,b)=>a+b);
+// let sumasNotas = notas.reduce((a,b)=>a+b);
 
-console.log(sumasNotas);
+// console.log(sumasNotas);
 
-notas.forEach((valor,indice) => console.log(('valor: '+valor+' indice: '+indice)));
+// notas.forEach((valor,indice) => console.log(('valor: '+valor+' indice: '+indice)));
+
+// //---------------------------------------------------------
+// let estudiantes = [
+//     {nombre: 'John', promedio: 8.5, aprobado: true},
+//     {nombre: 'Jane', promedio: 7, aprobado: true},
+//     {nombre: 'June', promedio: 3, aprobado: false},
+// ]
+
+// let aprobados = estudiantes.filter(function(estudiantes){
+//     if (estudiantes.aprobado == true){
+//         return estudiantes.nombre;
+//     }
+// })
+// let desaprobados =  estudiantes.filter(function(estudiantes){
+//     if(estudiantes.aprobado ==false){
+//         return estudiantes.nombre;
+//     }
+// })
+
+// console.log(desaprobados);
+// console.log(aprobados);
+
+// //---------------------------------------------------------
 
 
 
