4a2e7a8f890a2b8a3351f390c188ad66736492af
kiaruh <nikoz.li@gmail.com>
2021-09-13 18:22:01 -0300

    practica componentes

---
+++ claseReact/app/src/components/Pelicula.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Pelicula(){
+    return (
+        <div>
+        <h2>Titulo de la pelicula</h2>
+        <p>Ratings</p>
+        </div>
+    )
+}
+
+
+export default Pelicula;
---
+++ claseReact/app/src/components/TiraPeliculas.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Pelicula from "./Pelicula";
+
+function TiraPeliculas (){
+    return (
+        <div>
+        <Pelicula />
+        <Pelicula />
+        <Pelicula />
+        <Pelicula />
+        <Pelicula />
+        </div>
+    )
+
+}
+
+export default TiraPeliculas
