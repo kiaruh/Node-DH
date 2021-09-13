ef84d4f33b673aba02990728eb50a9312a7e09dd
kiaruh <nikoz.li@gmail.com>
2021-09-13 18:30:26 -0300

    practica JSX

--- claseReact/app/src/components/Pelicula.js
+++ claseReact/app/src/components/Pelicula.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 function Pelicula(){
+
+    let rating = 5.8;
+    let generos = ['Accion', 'Drama']
+
     return (
         <div>
-        <h2>Titulo de la pelicula</h2>
-        <p>Ratings</p>
+        <h2 className='titulo' >Titulo de la pelicula</h2>
+        <p>Rating: {rating}</p>
+        <ul>
+            { generos.map(genero => <li>{genero}</li>    ) }
+        </ul>
         </div>
     )
 }
