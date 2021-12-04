58726c852b0e4a7e39665af06fb48bda29dbc117
kiaruh <nikoz.li@gmail.com>
2021-12-04 16:29:55 -0300

    calculadora

---
+++ clase3Calculadora/calculadoraAdri.js
@@ -0,0 +1,28 @@
+const operaciones = {
+    suma: (a, b) => a + b,
+    resta: (a, b) => a - b,
+    multiplicacion: (a, b) => a * b,
+}
+
+let calculadora1 = (num1, num2, operacion) => {
+    return operaciones[operacion](num1, num2);
+}
+
+console.log('usando objeto: ' + calculadora1(2, 3, 'suma'));
+console.log('usando objeto: ' + calculadora1(2, 3, 'resta'));
+
+
+function calculadora2 (num1, num2, operacion) {
+    switch (operacion) {
+        case 'suma': return operaciones.suma(num1, num2);
+            
+        case 'resta': return operaciones.resta(num1, num2);
+           
+        case 'multiplicacion':return operaciones.multiplicacion(num1, num2);
+            
+        default: return 'operacion no valida';
+    }
+}
+
+console.log('usando switch: ' + calculadora2(5, 3, 'resta'));
+console.log('usando switch: ' + calculadora2(5, 3, 'asd'));
